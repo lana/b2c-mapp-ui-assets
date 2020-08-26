d25aac865f12da4bbb1b88bd17057e3386476aa8
Jason Dujardin-Terry <jason.dujardin@lana.xyz>
2020-08-26 15:33:52 +0200

    Updated some icons from new exported SVG files (from Illustrator instead of from Figma).

--- src/components/ColorLockIcon/ColorLockIcon.vue
+++ src/components/ColorLockIcon/ColorLockIcon.vue
@@ -1,54 +1,145 @@
 <template>
-  <svg class="icon" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <mask id="path-1-inside-1" fill="white">
-      <path d="M15 20.8242V13.2308C15 8.13363 19.1336 4 24.2308 4C29.3279 4 33.4615 8.13363 33.4615 13.2308V20.8242"/>
-    </mask>
-    <path d="M12 20.8242C12 22.481 13.3431 23.8242 15 23.8242C16.6569 23.8242 18 22.481 18 20.8242H12ZM30.4615 20.8242C30.4615 22.481 31.8047 23.8242 33.4615 23.8242C35.1184 23.8242 36.4615 22.481 36.4615 20.8242H30.4615ZM18 20.8242V13.2308H12V20.8242H18ZM18 13.2308C18 9.79048 20.7905 7 24.2308 7V1C17.4768 1 12 6.47677 12 13.2308H18ZM24.2308 7C27.6711 7 30.4615 9.79048 30.4615 13.2308H36.4615C36.4615 6.47677 30.9848 1 24.2308 1V7ZM30.4615 13.2308V20.8242H36.4615V13.2308H30.4615Z" fill="#DFE1E6" mask="url(#path-1-inside-1)"/>
-    <rect x="9"
-          y="20"
-          width="30"
-          height="24"
-          rx="4"
-          fill="#FAC34B"
-    />
-    <mask id="mask0"
-          mask-type="alpha"
-          maskUnits="userSpaceOnUse"
-          x="9"
-          y="20"
-          width="30"
-          height="24"
-    >
-      <rect x="9"
-            y="20"
-            width="30"
-            height="24"
-            rx="4"
-            fill="#FAC34B"
-      />
-    </mask>
-    <g mask="url(#mask0)">
-      <path opacity="0.5" d="M39 44V20L9 44H39Z" fill="url(#paint0_linear)"/>
-    </g>
-    <path d="M24 27C25.6575 27 27 28.3425 27 30C27 31.6575 25.6575 33 24 33C22.3425 33 21 31.6575 21 30C21 28.3425 22.3425 27 24 27Z"
-          fill="#C87C1B"
-          stroke="#C87C1B"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-    />
-    <path d="M22.5 32.5H25.5V37C25.5 37.8284 24.8284 38.5 24 38.5C23.1716 38.5 22.5 37.8284 22.5 37V32.5Z" fill="#C87C1B" stroke="#C87C1B"/>
+  <svg id="Capa_1"
+       class="icon"
+       data-name="Capa 1"
+       xmlns="http://www.w3.org/2000/svg"
+       xmlns:xlink="http://www.w3.org/1999/xlink"
+       viewBox="0 0 40.797 44"
+  >
     <defs>
-      <linearGradient id="paint0_linear"
-                      x1="39"
-                      y1="33"
-                      x2="3"
-                      y2="50.5"
+      <clipPath id="clip-path" transform="translate(0.4 2)">
+        <rect width="40" height="40" fill="none"/>
+      </clipPath>
+      <linearGradient id="Degradado_sin_nombre"
+                      data-name="Degradado sin nombre"
+                      x1="19.999"
+                      y1="59.39"
+                      x2="19.998"
+                      y2="-0.11"
+                      gradientTransform="matrix(1, 0, 0, -1, 0, 39.89)"
                       gradientUnits="userSpaceOnUse"
       >
-        <stop stop-color="#FFDC84"/>
-        <stop offset="1" stop-color="#FBD677" stop-opacity="0"/>
+        <stop offset="0" stop-color="#adaded"/>
+        <stop offset="1" stop-color="#7878c9"/>
       </linearGradient>
+      <mask id="mask"
+            x="0"
+            y="0"
+            width="40.797"
+            height="44"
+            maskUnits="userSpaceOnUse"
+      >
+        <g transform="translate(0.4 2)">
+          <g id="path-3-inside-1">
+            <path d="M35.907,3.12A40.264,40.264,0,0,0,20,0,40.273,40.273,0,0,0,4.088,3.12,4.123,4.123,0,0,0,1.6,6.887V19.059A21.409,21.409,0,0,0,15.786,38.9l1.788.667a7.025,7.025,0,0,0,4.851,0l1.784-.667A21.4,21.4,0,0,0,38.4,19.059V6.887a4.12,4.12,0,0,0-2.49-3.767Z" fill="#fff"/>
+          </g>
+        </g>
+      </mask>
+      <mask id="mask-2"
+            x="14.7"
+            y="9.575"
+            width="11.619"
+            height="10.842"
+            maskUnits="userSpaceOnUse"
+      >
+        <g transform="translate(0.4 2)">
+          <g id="path-4-inside-2">
+            <path d="M15.725,16.991V13.385a4.385,4.385,0,1,1,8.769,0v3.606" fill="#fff"/>
+          </g>
+        </g>
+      </mask>
+      <mask id="mask-3"
+            x="13.275"
+            y="18.6"
+            width="14.25"
+            height="11.4"
+            maskUnits="userSpaceOnUse"
+      >
+        <g transform="translate(0.4 2)">
+          <g id="mask0">
+            <rect x="12.875"
+                  y="16.6"
+                  width="14.25"
+                  height="11.4"
+                  rx="1.9"
+                  fill="#fac34b"
+            />
+          </g>
+        </g>
+      </mask>
+      <linearGradient id="Degradado_sin_nombre_2"
+                      data-name="Degradado sin nombre 2"
+                      x1="26.938"
+                      y1="17.499"
+                      x2="9.838"
+                      y2="9.186"
+                      gradientTransform="matrix(1, 0, 0, -1, 0, 39.89)"
+                      gradientUnits="userSpaceOnUse"
+      >
+        <stop offset="0" stop-color="#ffdc84"/>
+        <stop offset="1" stop-color="#fbd677" stop-opacity="0"/>
+      </linearGradient>
+      <mask id="mask-4"
+            x="17.55"
+            y="20.5"
+            width="5.7"
+            height="5.7"
+            maskUnits="userSpaceOnUse"
+      >
+        <g transform="translate(0.4 2)">
+          <g id="path-9-inside-3">
+            <path d="M20,19.45a1.9,1.9,0,1,1-1.9,1.9A1.9,1.9,0,0,1,20,19.45Z" fill="#fff"/>
+          </g>
+        </g>
+      </mask>
+      <mask id="mask-5"
+            x="18.975"
+            y="23.825"
+            width="2.85"
+            height="4.275"
+            maskUnits="userSpaceOnUse"
+      >
+        <g transform="translate(0.4 2)">
+          <g id="path-10-inside-4">
+            <path d="M19.05,22.3h1.9v2.375a.95.95,0,0,1-1.9,0Z" fill="#fff"/>
+          </g>
+        </g>
+      </mask>
     </defs>
+    <g clip-path="url(#clip-path)">
+      <g>
+        <path d="M35.907,3.12A40.264,40.264,0,0,0,20,0,40.273,40.273,0,0,0,4.088,3.12,4.123,4.123,0,0,0,1.6,6.887V19.059A21.409,21.409,0,0,0,15.786,38.9l1.788.667a7.025,7.025,0,0,0,4.851,0l1.784-.667A21.4,21.4,0,0,0,38.4,19.059V6.887a4.12,4.12,0,0,0-2.49-3.767Z" transform="translate(0.4 2)" fill="url(#Degradado_sin_nombre)"/>
+        <path d="M4.088,3.12A40.273,40.273,0,0,1,20,0l0,40a7.025,7.025,0,0,1-2.426-.432L15.786,38.9A21.409,21.409,0,0,1,1.6,19.059V6.887A4.125,4.125,0,0,1,4.088,3.12Z" transform="translate(0.4 2)" fill="#7878c9" opacity="0.5" style="isolation: isolate"/>
+        <g mask="url(#mask)">
+          <path d="M35.907,3.12A40.264,40.264,0,0,0,20,0,40.273,40.273,0,0,0,4.088,3.12,4.123,4.123,0,0,0,1.6,6.887V19.059A21.409,21.409,0,0,0,15.786,38.9l1.788.667a7.025,7.025,0,0,0,4.851,0l1.784-.667A21.4,21.4,0,0,0,38.4,19.059V6.887a4.12,4.12,0,0,0-2.49-3.767Z" transform="translate(0.4 2)" fill="none" stroke="#ebecf0" stroke-width="4"/>
+        </g>
+        <g mask="url(#mask-2)">
+          <path d="M14.3,16.991a1.425,1.425,0,0,0,2.85,0Zm8.769,0a1.425,1.425,0,0,0,2.85,0Zm-5.919,0V13.385H14.3v3.606Zm0-3.606a2.96,2.96,0,0,1,2.96-2.96V7.575a5.81,5.81,0,0,0-5.81,5.81Zm2.96-2.96a2.959,2.959,0,0,1,2.959,2.96h2.85a5.809,5.809,0,0,0-5.809-5.81Zm2.959,2.96v3.606h2.85V13.385Z" transform="translate(0.4 2)" fill="#dfe1e6"/>
+        </g>
+        <rect x="13.275"
+              y="18.6"
+              width="14.25"
+              height="11.4"
+              rx="1.9"
+              fill="#fac34b"
+        />
+        <g mask="url(#mask-3)">
+          <path d="M27.125,28V16.6L12.875,28Z" transform="translate(0.4 2)" opacity="0.5" fill="url(#Degradado_sin_nombre_2)" style="isolation: isolate"/>
+        </g>
+        <g mask="url(#mask-4)">
+          <path d="M20,19.45a1.9,1.9,0,1,1-1.9,1.9A1.9,1.9,0,0,1,20,19.45Z"
+                transform="translate(0.4 2)"
+                fill="#c87c1b"
+                stroke="#c87c1b"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="1.9"
+          />
+        </g>
+        <g mask="url(#mask-5)">
+          <path d="M19.05,22.3h1.9v2.375a.95.95,0,0,1-1.9,0Z" transform="translate(0.4 2)" fill="#c87c1b" stroke="#c87c1b" stroke-width="0.95"/>
+        </g>
+      </g>
+    </g>
   </svg>
 </template>
